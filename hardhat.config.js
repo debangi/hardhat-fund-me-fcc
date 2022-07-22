67e95b3949df614d569f0f95d5cbd5aae075fe74
Debangi <debangiofficial@gmail.com>
2022-07-22 22:43:35 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("dotenv").config();
+
+const RINKEBY_RPC_URL =
+    process.env.RINKEBY_RPC_URL || "https://eth-rinkeby/example";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        // kovan: {
+        //     url: KOVAN_RPC_URL,
+        //     accounts: [PRIVATE_KEY],
+        //     chainId: 42,
+        //     blockConfirmations: 6,
+        //     gas: 6000000,
+        // },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+    },
+    // solidity: "0.8.9",
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0, //chainId : indexId
+        },
+    },
+};
